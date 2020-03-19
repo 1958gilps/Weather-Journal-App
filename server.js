d95d45c1b14c2f0ed6cd5f8344d8a052edd082be
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-03-19 08:03:02 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    console.log('running on a server');
+    console.log(`running on localhost: ${port}`);
+  };
+
+app.get('/test',(req,res)=>{
+  res.send("Hi, the server is still working...")
+})
